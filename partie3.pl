e931e99f72c8cec802435287ee2658b8fd414a94
Raphaël R <93404597+Raphael-Renard@users.noreply.github.com>
2023-12-17 19:19:27 +0100

    Update partie3.pl

--- partie3.pl
+++ partie3.pl
@@ -1,3 +1,11 @@
+:- encoding(utf8).
+
+
+concept(C) :- cnamea(C). % à enlever après ajout de partie 1
+concept(CG) :- cnamena(CG). % à enlever après ajout de partie 1
+
+
+
 %/* Tri Abox %/*
 
 tri_Abox([],[],[],[],[],[]).
@@ -65,6 +73,10 @@ evolue((I,C), Lie, Lpt, Li, Lu, Ls, Lie, Lpt, Li, Lu, Ls):-
     cnamea(C).
 
 
+evolue_plusieurs([], Lie, Lpt, Li, Lu, Ls, Lie, Lpt, Li, Lu, Ls).
+evolue_plusieurs([A|L], Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, Ls1) :-
+    evolue(A, Lie, Lpt, Li, Lu, Ls, Lie2, Lpt2, Li2, Lu2, Ls2),
+    evolue_plusieurs(L, Lie2, Lpt2, Li2, Lu2, Ls2, Lie1, Lpt1, Li1, Lu1, Ls1).
 
 
 
@@ -81,7 +93,7 @@ complete_some([(A,some(R,C)) | Lie], Lpt, Li, Lu, Ls, Abr):-
     evolue((B, C), Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, Ls1),
     
     % affichage
-    affiche_evolution_Abox(Ls, [(A,some(R,C)) | Lie], Lpt, Li, Lu, Abr, Ls1, Lie1, Lpt1, Li1, Lu1, Abr1),      
+    affiche_evolution_Abox(Ls, [(A,some(R,C)) | Lie], Lpt, Li, Lu, Abr, Ls1, Lie1, Lpt1, Li1, Lu1, Abr),      
 
     % ajoute assertion <a, b> : R et continue la résolution
     resolution(Lie1, Lpt1, Li1, Lu1, Ls1, [(A, B, R) | Abr]).               
@@ -89,30 +101,46 @@ complete_some([(A,some(R,C)) | Lie], Lpt, Li, Lu, Ls, Abr):-
 
 /*Règle ⊓*/
 transformation_and(Lie, Lpt, [(A,and(C,D)) | Li], Lu, Ls, Abr):-
-    % ajoute assertion a : C
-    evolue((A, C), Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, Ls1),
-
-    % ajoute assertion a : D
-    evolue((A, D), Lie1, Lpt1, Li1, Lu1, Ls1, Lie2, Lpt2, Li2, Lu2, Ls2), 
+    % ajoute assertion a : c et a : d
+    evolue_plusieurs([(A, C),(A, D)], Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, Ls1),
 
     % affichage
-    affiche_evolution_Abox(Ls, Lie, Lpt, [(A,and(C,D)) | Li], Lu, Abr, Ls2, Lie2, Lpt2, Li2, Lu2, Abr2),
-
-    % continue la résolution
-    resolution(Lie2, Lpt2, Li2, Lu2, Ls2, Abr). 
-
-
-/*Règle ∀*/
-deduction_all(Lie,[(A,all(R,C)) | Lpt], Li, Lu, Ls, [(A, B, R) | Abr]):- 
-    % ajoute assertion b : C
-    evolue((B, C), Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, Ls1),
-
-    % affichage
-    affiche_evolution_Abox(Ls, Lie, [(A,all(R,C)) | Lpt], Li, Lu, Abr, Ls1, Lie1, Lpt1, Li1, Lu1, Abr1),
+    affiche_evolution_Abox(Ls, Lie, Lpt, [(A,and(C,D)) | Li], Lu, Abr, Ls1, Lie1, Lpt1, Li1, Lu1, Abr),
 
     % continue la résolution
     resolution(Lie1, Lpt1, Li1, Lu1, Ls1, Abr). 
 
+
+/*Règle ∀*/
+deduction_all(Lie,[(A,all(R,C)) | Lpt], Li, Lu, Ls, Abr):- 
+    nl,
+
+    % crée liste L_BC pour avec tous les B apparaissant dans une assertion du type (A,B,R)
+    setof((B, C),  member((A, B, R), Abr), L_BC),
+
+    % crée liste L_ABR de toutes les assertions du type (A,B,R)
+    setof((A, B, R),  member((A, B, R), Abr), L_ABR),
+
+    % affichage des instances sur lequelles on applique la règle
+    affichage_forall(L_ABR),
+
+
+    % ajoute assertions b : c
+    evolue_plusieurs(L_BC, Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, Ls1),
+
+    % affichage
+    affiche_evolution_Abox(Ls, Lie, [(A,all(R,C)) | Lpt], Li, Lu, Abr, Ls1, Lie1, Lpt1, Li1, Lu1, Abr),
+
+    % continue la résolution
+    resolution(Lie1, Lpt1, Li1, Lu1, Ls1, Abr).
+
+
+
+deduction_all(Lie,[(A,all(R,C)) | Lpt], Li, Lu, Ls, Abr):- 
+    \+ member((A,B,C),Abr), 
+
+    % continue la résolution
+    resolution(Lie, Lpt, Li, Lu, Ls, Abr).
 
 
 /* Règle ⊔*/
@@ -121,21 +149,26 @@ transformation_or(Lie, Lpt, Li, [(A,or(C,D)) | Lu], Ls, Abr):-
     evolue((A, C), Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, Ls1),
 
     % affichage
-    affiche_evolution_Abox(Ls, Lie, Lpt, Li, [(A,or(C,D)) | Lu], Abr, Ls1, Lie1, Lpt1, Li1, Lu1, Abr1),
+    nl,
+    write("On applique la règle  \u2294 sur "), affichage([(A,or(C,D))]),nl,
+    affiche_evolution_Abox(Ls, Lie, Lpt, Li, [(A,or(C,D)) | Lu], Abr, Ls1, Lie1, Lpt1, Li1, Lu1, Abr).
 
+    /*
     % continue la résolution de la première branche
     resolution(Lie1, Lpt1, Li1, Lu1, Ls1, Abr).
-
+    */
 
 transformation_or(Lie, Lpt, Li, [(A,or(C,D)) | Lu], Ls, Abr):-
     % ajoute assertion a : D à la deuxième branche
     evolue((A, D), Lie, Lpt, Li, Lu, Ls, Lie2, Lpt2, Li2, Lu2, Ls2),
 
     % affichage
-    affiche_evolution_Abox(Ls, Lie, Lpt, Li, [(A,or(C,D)) | Lu], Abr, Ls2, Lie2, Lpt2, Li2, Lu2, Abr2),
+    affiche_evolution_Abox(Ls, Lie, Lpt, Li, [(A,or(C,D)) | Lu], Abr, Ls2, Lie2, Lpt2, Li2, Lu2, Abr).
 
+    /*
     % continue la résolution de la deuxième branche
     resolution(Lie2, Lpt2, Li2, Lu2, Ls2, Abr).
+    */
 
 
 
@@ -143,23 +176,32 @@ transformation_or(Lie, Lpt, Li, [(A,or(C,D)) | Lu], Ls, Abr):-
 
 
 %/* Affichage */% 
+
+affichage_forall([]).
+affichage_forall([(A,B,R)|L_instR]):- 
+    write("On applique la règle \u2200 sur "), affichageR([(A,B,R)]), nl,
+    affichage_forall(L_instR).
+
+
 affichage([]).
 
-affichage(C) :- cnamea(C), write(C).
-affichage(not(C)):- write("¬ "), affichage(C).
-affichage(or(C1, C2)) :- affichage(C1), write(' ⊔ '), affichage(C2).
-affichage(and(C1, C2)) :- affichage(C1), write(' ⊓ '), affichage(C2).
-affichage(some(R, C)) :- write('∃ '), write(R), write('.'), affichage(C).
-affichage(all(R, C)) :- write('∀ '), write(R), write('.'), affichage(C).
+affichage(C) :- concept(C), write(C).
+affichage(I) :- iname(I), write(I).
+affichage(not(C)) :- write(" \u00AC "), affichage(C). % ¬
+affichage(or(C1, C2)) :- affichage(C1), write(' \u2294 '), affichage(C2). % ⊔
+affichage(and(C1, C2)) :- affichage(C1), write(' \u2293 '), affichage(C2). % ⊓
+affichage(some(R, C)) :- write(' \u2203 '), write(R), write('.'), affichage(C). % ∃
+affichage(all(R, C)) :- write(' \u2200 '), write(R), write('.'), affichage(C). % ∀
 
 affichage([(I, C) | L]):-
-	write(I), write(' : '), affichage(C),nl,
+	write(I), write(' : '), affichage(C), nl,
 	affichage(L).
 
-affichage([(I1, I2, R) | L]) :-
+affichageR([]).
+affichageR([(I1, I2, R) | L]) :-
 	write('<'), write(I1), write(', '), write(I2), write('> : '),
 	write(R),nl,
-	affichage(L).
+	affichageR(L).
 
 affiche_evolution_Abox(Ls1, Lie1, Lpt1, Li1, Lu1, Abr1, Ls2, Lie2, Lpt2, Li2, Lu2, Abr2):-
     write("Etat de départ :"),nl,nl,
@@ -168,7 +210,7 @@ affiche_evolution_Abox(Ls1, Lie1, Lpt1, Li1, Lu1, Abr1, Ls2, Lie2, Lpt2, Li2, Lu
     affichage(Lpt1),
     affichage(Li1),
     affichage(Lu1),
-    affichage(Abr1),
+    affichageR(Abr1),
     nl,
 
     write("Etat d'arrivée :"),nl,nl,
@@ -177,7 +219,7 @@ affiche_evolution_Abox(Ls1, Lie1, Lpt1, Li1, Lu1, Abr1, Ls2, Lie2, Lpt2, Li2, Lu
     affichage(Lpt2),
     affichage(Li2),
     affichage(Lu2),
-    affichage(Abr2),
+    affichageR(Abr2),
     nl,
 
     (test_clash(Ls2)->write('Il y a un clash. La branche est fermée.'); write('Pas de clash')),
