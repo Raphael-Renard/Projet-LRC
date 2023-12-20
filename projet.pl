2fac665063734a4652b5bbefffa0e6ba1ce3e79c
Raphaël R <93404597+Raphael-Renard@users.noreply.github.com>
2023-12-20 21:56:44 +0100

    Update projet.pl

--- projet.pl
+++ projet.pl
@@ -1,41 +1,6 @@
 /* Fonctions données dans l'énoncé */
 
-concat([],L1,L1).
-concat([X|Y],L1,[X|L2]) :- concat(Y,L1,L2).
 
-enleve(X,[X|L],L) :-!.
-enleve(X,[Y|L],[Y|L2]) :- enleve(X,L,L2).
-
-
-compteur(1).
-genere(Nom) :- compteur(V),nombre(V,L1),
-                concat([105,110,115,116],L1,L2),
-                V1 is V+1,
-                dynamic(compteur/1),
-                retract(compteur(V)),
-                dynamic(compteur/1),
-                assert(compteur(V1)),nl,nl,nl,
-                name(Nom,L2).
-
-
-nombre(0,[]).
-nombre(X,L1) :- R is (X mod 10),
-                Q is ((X-R)//10),
-                chiffre_car(R,R1),
-                char_code(R1,R2),
-                nombre(Q,L),
-                concat(L,[R2],L1).
-
-chiffre_car(0,'0').
-chiffre_car(1,'1').
-chiffre_car(2,'2').
-chiffre_car(3,'3').
-chiffre_car(4,'4').
-chiffre_car(5,'5').
-chiffre_car(6,'6').
-chiffre_car(7,'7').
-chiffre_car(8,'8').
-chiffre_car(9,'9').
 
 concat([],L1,L1).
 concat([X|Y],L1,[X|L2]) :- concat(Y,L1,L2).
@@ -175,7 +140,7 @@ acquisition_prop_type1(Abi, Abi1, Tbox) :-
     (instanciationC(I, CG) -> % if 
         writef('%w : %w', [I, CG])
         ; ( % else
-        write('\[ERREUR] : I est pas déclaré ou C est pas un concept'), nl,
+        write('[ERREUR] : I est pas déclaré ou C est pas un concept'), nl,
         write('A nouveau'), nl, fail
     )),
     transforme([(I,not(CG))], [(I, CG_dev_nnf)]), % Développement + nnf
@@ -466,7 +431,7 @@ premiere_etape(Tbox, Abi, Abr) :-
     setof((CA, CG), equiv(CA, CG), Tboxt),      
     setof((I1, I2), inst(I1, I2), Abit),        
     setof((I1, I2, R), instR(I1, I2, R), Abr), 
-    (verif_Tbox(Tboxt) ->
+    (verify_tbox(Tboxt) ->
         write('[LOG] Vérification de la TBox réussi'), nl;
         write('[ERREUR] Il y a erreur de syntaxe dans la TBox'), nl, halt),
     
